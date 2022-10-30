9ef4ca468bca5ab36714aa065a673a0e50a20197
max <me@maxlibin.com>
2022-10-30 17:23:30 +0800

    Add bg to pre tag for code preview

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -21,9 +21,9 @@ const Blog = ({data}: post) => {
   return (
     <Layout>
       <div className="mt-8">
-        <h1 className="text-6xl font-bold text-gray-900 dark:text-white py-8">@maxlibin - blog</h1>
+        <h1 className="text-6xl font-bold text-gray-900 dark:text-white inline-block py-8 mb-4">@maxlibin - blog</h1>
         {data.allWpPost.nodes.map(({slug, title, excerpt, date}) => (
-          <div className="mt-10 text-gray-900 dark:text-gray-300 text-xl md:max-w-4xl" key={slug}>
+          <div className="mb-10 text-gray-900 dark:text-gray-300 text-xl md:max-w-4xl" key={slug}>
             <div className="text-xs text-gray-700 dark:text-gray-500">{date}</div>
             <h3 className="text-2xl font-bold text-gray-900 dark:text-white mb-4 hover:text-indigo-500 dark:hover:text-indigo-700">
               <Link to={`/${slug}`}>
@@ -31,7 +31,7 @@ const Blog = ({data}: post) => {
               </Link>
             </h3>
             <div className="mb-4 flex h-1 w-[calc(100%+4rem)] bg-gradient-to-r from-indigo-400 via-green-500 to-pink-500 sm:mx-0 sm:w-full"></div>
-            <div className="text-xl text-gray-900 dark:text-gray-400 mb-2" dangerouslySetInnerHTML={{__html: excerpt}} />
+            <div className="text-sm text-gray-900 dark:text-gray-400 mb-2" dangerouslySetInnerHTML={{__html: excerpt}} />
           </div>
         ))}
       </div>
@@ -45,7 +45,7 @@ export const pageQuery = graphql`
       nodes {
         title
         excerpt
-        date
+        date(formatString: "MMMM DD, YYYY")
         slug
       }
     }
