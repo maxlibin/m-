bfefa821c89b32dfdd26938c3be95abb9b07a418
max <me@maxlibin.com>
2022-10-30 16:57:22 +0800

    Add blog page

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,10 +1,55 @@
 import React from "react"
+import {Link, graphql} from "gatsby"
 import Layout from "../components/layout"
 
-const Blog = () => {
+type node = {
+  date: string,
+  excerpt: string,
+  slug: string,
+  title: string
+}
+
+type post = {
+  data: {
+    allWpPost: {
+      nodes: Array<node>
+    }
+  }
+}
+
+const Blog = ({data}: post) => {
   return (
-    <Layout>Hello blog</Layout>
+    <Layout>
+      <div className="mt-8">
+        <h1 className="text-6xl font-bold text-gray-900 dark:text-white py-8">@maxlibin - blog</h1>
+        {data.allWpPost.nodes.map(({slug, title, excerpt, date}) => (
+          <div className="mt-10 text-gray-900 dark:text-gray-300 text-xl md:max-w-4xl" key={slug}>
+            <div className="text-xs text-gray-700 dark:text-gray-500">{date}</div>
+            <h3 className="text-2xl font-bold text-gray-900 dark:text-white mb-4 hover:text-indigo-500 dark:hover:text-indigo-700">
+              <Link to={`/${slug}`}>
+                {title}
+              </Link>
+            </h3>
+            <div className="mb-4 flex h-1 w-[calc(100%+4rem)] bg-gradient-to-r from-indigo-400 via-green-500 to-pink-500 sm:mx-0 sm:w-full"></div>
+            <div className="text-xl text-gray-900 dark:text-gray-400 mb-2" dangerouslySetInnerHTML={{__html: excerpt}} />
+          </div>
+        ))}
+      </div>
+    </Layout>
   )
 }
+
+export const pageQuery = graphql`
+  query {
+    allWpPost(sort: { fields: [date], order: DESC }) {
+      nodes {
+        title
+        excerpt
+        date
+        slug
+      }
+    }
+  }
+`
 
 export default Blog;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,8 +19,8 @@ type post = {
 const Index = ({data}: post) => {
   return (
     <Layout>
-      <div>
-        <h1 className="text-6xl font-bold text-gray-900 dark:text-white">@maxlibin</h1>
+      <div className=" mt-8">
+        <h1 className="text-6xl font-bold text-gray-900 dark:text-white pt-8">@maxlibin</h1>
         <p className="text-gray-600 dark:text-gray-400 py-2">I am based in Singapore, have Javascript, React, ReasonMl, ReasonReact projects you'd like to discuss?</p>
         <div className="mt-6">
           <h2 className="text-2xl text-gray-900 dark:text-white mb-4">Latest posts</h2>
