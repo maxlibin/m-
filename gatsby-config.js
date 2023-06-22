86473042bbc931da9c35f42308e773ffdb67b7eb
max <max.li.bin@ahrefs.com>
2023-06-22 12:22:50 +0800

    Enable robot and sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,5 +45,13 @@ module.exports = {
         ]
       },
     },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: 'https://www.maxlibin.com',
+        sitemap: 'https://www.maxlibin.com/sitemap.xml',
+        policy: [{userAgent: '*', allow: ''}]
+      }
+    }
   ],
 }
